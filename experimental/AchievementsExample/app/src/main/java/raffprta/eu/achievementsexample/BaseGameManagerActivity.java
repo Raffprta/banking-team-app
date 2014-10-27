bb9234eab6db64dc2ce3029e6f19c283ec1ee5c6
Raffprta <raffprta@gmail.com>
2014-10-27 23:06:02 +0000

    Fix error message to notify immediately after sign in has failed

--- experimental/AchievementsExample/app/src/main/java/raffprta/eu/achievementsexample/BaseGameManagerActivity.java
+++ experimental/AchievementsExample/app/src/main/java/raffprta/eu/achievementsexample/BaseGameManagerActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -99,7 +100,7 @@ public class BaseGameManagerActivity extends Activity implements GoogleApiClient
 
     @Override
     public void onConnectionSuspended(int i) {
-        // Attempt reconnection, if fails it should be delegated to onConenctionFailed()
+        // Attempt reconnection, if fails it should be delegated to onConnectionFailed()
         mGoogleApiClient.connect();
     }
 
@@ -138,6 +139,8 @@ public class BaseGameManagerActivity extends Activity implements GoogleApiClient
                 if (!mGoogleApiClient.isConnecting() && !mGoogleApiClient.isConnected()) {
                     mGoogleApiClient.connect();
                 }
+            }else{
+                displayErrorMessage();
             }
         }
     }
