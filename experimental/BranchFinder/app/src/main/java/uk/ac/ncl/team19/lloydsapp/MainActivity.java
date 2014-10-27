173da1a7e541a3fecd34539ba2d2168fe65610be
Dale Whinham <daleyo@gmail.com>
2014-10-27 21:20:13 +0000

    Add experimental Branch Finder code

---
+++ experimental/BranchFinder/app/src/main/java/uk/ac/ncl/team19/lloydsapp/MainActivity.java
@@ -0,0 +1,346 @@
+package uk.ac.ncl.team19.lloydsapp;
+
+import android.app.Activity;
+import android.location.Address;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.CookieManager;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+    // Types of search query
+    private static final int QUERY_ATM = 0;
+    private static final int QUERY_BRANCH = 1;
+
+    // Constants
+    private static final String BRANCH_FINDER_POST_URL = "http://www.lloydsbank.com/branch_locator/application.asp?inapplication=yes";
+    private static final String BRANCH_FINDER_REFERER_URL = "http://www.lloydsbank.com/branch_locator/search.asp";
+
+    // Logging tag
+    private final String TAG = getClass().getSimpleName();
+
+    // Location
+    private Location myLocation = null;
+
+    // Google Map
+    private GoogleMap map = null;
+
+    // List of branches
+    private List<Branch> branches;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Enable ActionBar progress bar
+        requestWindowFeature(Window.FEATURE_PROGRESS);
+
+        // Apply layout
+        setContentView(R.layout.activity_main);
+
+        // Enable cookie storage
+        CookieManager.setDefault(new CookieManager());
+
+        // References to UI elements
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.googleMap)).getMap();
+        final Button findBranchesButton = (Button) findViewById(R.id.findBranchesButton);
+
+        // Set button disabled until location is determined
+        findBranchesButton.setEnabled(false);
+
+        // Map will try to determine location
+        map.setMyLocationEnabled(true);
+
+        Log.i(TAG, "Internet available? " + Utility.deviceHasInternetConnection(this));
+
+        map.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                // Store location
+                myLocation = location;
+
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                String postcode = Utility.locationToPostcode(getApplicationContext(), location);
+
+                if (postcode != null) {
+                    Toast.makeText(getApplicationContext(), "Your postcode is roughly: " + postcode, Toast.LENGTH_LONG).show();
+                }
+
+                // Disable listener
+                map.setOnMyLocationChangeListener(null);
+                map.setMyLocationEnabled(false);
+
+                map.addMarker(new MarkerOptions()
+                        .title("Your Location")
+                        .snippet("A description.")
+                        .position(latLng));
+
+                map.moveCamera(CameraUpdateFactory.zoomBy(7));
+                map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                // Enable find button
+                findBranchesButton.setEnabled(true);
+            }
+        });
+
+        // Find button handler
+        findBranchesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Enable progress bar
+                setProgressBarIndeterminate(true);
+                setProgressBarVisibility(true);
+
+                QueryBranchLocationsTask qb = new QueryBranchLocationsTask();
+                qb.execute(myLocation);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private List<Branch> queryBranchFinder(int queryType, Location location) throws IOException {
+        // Attempt to convert location to postcode
+        String postcode = Utility.locationToPostcode(getApplicationContext(), location);
+
+        // Abort if we don't have a postcode to use
+        if (postcode == null) {
+            return null;
+        }
+
+        Log.i(TAG, "Postcode determined to be: " + postcode);
+
+        // Input stream for holding the response
+        InputStream inputStream = null;
+
+        // Output stream for sending the POST data
+        DataOutputStream dataOutputStream = null;
+
+        try {
+            URL url = new URL(BRANCH_FINDER_POST_URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000);     // milliseconds
+            conn.setConnectTimeout(15000);  // milliseconds
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+
+            // Lloyds branch finder requires referer to be set
+            conn.setRequestProperty("Referer", BRANCH_FINDER_REFERER_URL);
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+            // Search fields
+            String formFields = String.format(  "step=%d" +
+                                                "&bl_query_select=%d" +
+                                                "&bl_town=%s" +
+                                                "&bl_postcode=%s" +
+                                                "&bl_district=%s" +
+                                                "&bl_sort_by=%d" +
+                                                "&continue.x=%d" +
+                                                "&continue.y=%d" +
+                                                //"&continue=continue" +
+                                                "&hidden_xmas_opening=0",
+                                                1,
+                                                queryType,
+                                                "",
+                                                postcode.replaceAll("\\s", ""), // Remove whitespace from postcode
+                                                "",
+                                                0, 0, 0 );
+
+            // Replace all spaces with '+'
+            formFields = formFields.replaceAll(" ", "+");
+
+            Log.d(TAG, "Using form fields: " + formFields);
+
+            dataOutputStream = new DataOutputStream(conn.getOutputStream());
+            dataOutputStream.writeBytes(formFields);
+
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d(TAG, "The response was: " + response);
+
+            inputStream = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String responseBody = Utility.inputStreamToString(inputStream);
+
+            return parseResults(responseBody);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Close streams no matter what happens
+            if (inputStream != null) {
+                inputStream.close();
+            }
+
+            if (dataOutputStream != null) {
+                dataOutputStream.close();
+            }
+        }
+        return null;
+    }
+
+    private List<Branch> parseResults(String html) {
+        // List of parsed Branches
+        List<Branch> addresses = new ArrayList<Branch>();
+
+        // Parse HTML into DOM object
+        Document doc = Jsoup.parse(html);
+
+        // Search results are enclosed in <div class="line"> tags
+        Elements searchResults = doc.select("div.line");
+
+        // Progress bar no longer indeterminate
+        setProgressBarIndeterminate(false);
+
+        for (int i = 0; i < searchResults.size(); i++) {
+            /*
+            Lloyds branch finder returns results like this:
+
+            <div class="line">
+                <a href="...">3 Station Road Ashington</a>
+                <br>
+                3 Station Road, Ashington, Northumberland, NE63 9UZ
+            </div>
+             */
+
+            // Get the current element
+            Element e = searchResults.get(i);
+
+            // Update progress bar
+            setProgress(i * 10000 / searchResults.size());
+
+            // First line/branch name is enclosed in an <a> tag
+            String branchName = e.select("a")
+                    .first().text()             // the first <a> tag's inner text
+                    .replaceAll("\u00a0", " ")  // HTML &nbsp;s get converted to Unicode 0x00a0 character; replace them with ASCII spaces
+                    .trim();                    // remove leading and trailing spaces
+
+            // Split address lines by comma
+            String[] splitAddress = e.ownText().split(",");
+
+            // List of address lines
+            List<String> addressLines = new ArrayList<String>();
+
+            // Add all but last individual address lines to address line list
+            for (int j = 0; j < splitAddress.length - 1; j++) {
+                // Replace &nbsp;/Unicode 0x00a0 with ASCII space and trim leading/trailing spaces
+                String line = splitAddress[j].replaceAll("\u00a0", " ").trim();
+
+                // Store the address line
+                addressLines.add(line);
+            }
+
+            // Last address line is the postcode (hopefully!)
+            String postcode = splitAddress[splitAddress.length - 1];
+
+            // Clean up whitespace
+            postcode = postcode.replaceAll("\u00a0", " ").trim();
+
+            // Create a new branch object with the parsed data
+            Branch branch = new Branch(branchName, addressLines, postcode);
+
+            // Get location data from Geocoding API and store it with the Branch
+            Address a = Utility.branchToAddress(getApplicationContext(), branch);
+
+            if (a != null) {
+                branch.setLongitude(a.getLongitude());
+                branch.setLatitude(a.getLatitude());
+            } else {
+                Log.e(TAG, "Couldn't geocode branch: " + branch.toString());
+            }
+
+            // Add the Branch to the list
+            addresses.add(branch);
+        }
+
+        return addresses;
+    }
+
+    private class QueryBranchLocationsTask extends AsyncTask<Location, Void, List<Branch>> {
+        @Override
+        protected List<Branch> doInBackground(Location... locations) {
+            // Attempt to query the Lloyds Branch Finder
+            try {
+                return queryBranchFinder(QUERY_BRANCH, locations[0]);
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(List<Branch> results) {
+            // Stop progress bar
+            setProgressBarVisibility(false);
+
+            if (results == null) {
+                // Query failed
+                Toast.makeText(getApplicationContext(), "No branches found.", Toast.LENGTH_LONG).show();
+            } else {
+                // Query succeeded
+                Log.d (TAG, results.size() + " branches found!");
+
+                for (Branch b: results) {
+                    if (b.getLongitude() != 0 && b.getLatitude() != 0) {
+                        // Add map markers
+                        map.addMarker(new MarkerOptions()
+                                .title(b.getName())
+                                .snippet(b.getAddressLines().toString())
+                                .position(new LatLng(b.getLatitude(), b.getLongitude()))
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+                    }
+                }
+
+                // Store results
+                branches = results;
+            }
+        }
+    }
+}
---
+++ experimental/BranchFinder/app/src/main/java/uk/ac/ncl/team19/lloydsapp/Utility.java
@@ -0,0 +1,116 @@
+package uk.ac.ncl.team19.lloydsapp;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * Utility class with miscellaneous functions
+ * Created by Dale Whinham on 22/10/14.
+ */
+public class Utility {
+    // Tag for logging
+    private static final String TAG = Utility.class.getSimpleName();
+
+    /**
+     * Checks if the device has an Internet connection.
+     * @param ctx the current Application Context
+     * @return true if the device has an Internet connection available, false otherwise
+     */
+    public static boolean deviceHasInternetConnection(Context ctx) {
+        // Get system connectivity manager
+        ConnectivityManager connMgr = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Query system for Internet connectivity
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // True if Internet connection available
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    /**
+     * Converts a Location to a postcode using a Geocoder.
+     * @param ctx the current Application Context
+     * @param location the Location to convert
+     * @return the postcode as a String, null if it was unsuccessful
+     */
+    public static String locationToPostcode(Context ctx, Location location) {
+        // Return null if the Geocoder API is not functional (no backend present)
+        if (!Geocoder.isPresent()) {
+            return null;
+        }
+
+        Log.i(TAG, "Geocoder is present, querying...");
+        Geocoder geocoder = new Geocoder(ctx);
+
+        try {
+            // Get one address using the Geocoder API
+            List<Address> results = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+            // getFromLocation can return null or empty list
+            if (results != null && !results.isEmpty()) {
+                // We got a result, return its postcode
+                return results.get(0).getPostalCode();
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "An error occurred while geocoding: " + e.toString());
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * Converts an Address to a Location using a Geocoder.
+     * @param ctx the current Application Context
+     * @param branch the Branch to convert
+     * @return the postcode as a String, null if it was unsuccessful
+     */
+    public static Address branchToAddress(Context ctx, Branch branch) {
+        // Return null if the Geocoder API is not functional (no backend present)
+        if (!Geocoder.isPresent()) {
+            return null;
+        }
+
+        Log.i(TAG, "Geocoder is present, querying...");
+        Geocoder geocoder = new Geocoder(ctx);
+
+        try {
+            // Get one Location using the Geocoder API
+            List<Address> results = geocoder.getFromLocationName(branch.getLocationString(), 1);
+
+            Log.d(TAG, "Using '" + branch.getLocationString() + "' as query.");
+
+            // getFromLocationName can return null or empty list
+            if (results != null && !results.isEmpty()) {
+                // We got a result, return its Address
+                return results.get(0);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "An error occurred while geocoding: " + e.toString());
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * Attempts to convert an InputStream to a String.
+     * @param inputStream the InputStream
+     * @return A String object
+     * @throws IOException
+     */
+    public static String inputStreamToString(InputStream inputStream) throws IOException {
+        // Courtesy of http://stackoverflow.com/questions/309424/read-convert-an-inputstream-to-a-string
+        java.util.Scanner s = new java.util.Scanner(inputStream).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+}
