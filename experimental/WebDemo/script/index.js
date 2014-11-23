fd45a0a896773690f98ff29ef967c40832987443
Raffprta <raffprta@gmail.com>
2014-11-23 19:49:31 +0000

    Updated index page with slider and non-placeholder text

---
+++ experimental/WebDemo/script/index.js
@@ -0,0 +1,32 @@
+// Simple script for slider transitions.
+
+// The images directory.
+var directory = "images/";
+// Image array.
+var slider_images = ["slide_1.png", "slide_2.png", "slide_3.png", "slide_4.png", "slide_5.png"];
+// The position it is at currently in the slider.
+var transition_at = 0;
+
+var slider_increment = function(offset){
+    // Get the next slider image.
+    transition_at = (transition_at + offset) % slider_images.length;
+    // If the number falls below 0 (index out of bounds) set to the last array index.
+    if(transition_at < 0){
+        transition_at = slider_images.length - 1;
+    }
+    // JQuery animation!
+    $("#slider").slideDown("slow", function() {
+        $(this).attr("src", directory + slider_images[transition_at]);
+    });
+}
+
+
+// When the left button is clicked then go to the previous image.
+$( goLeft ).click(function() {
+    slider_increment(-1);
+});
+
+// When the right button is clicked then go to the next image.
+$( goRight ).click(function() {
+    slider_increment(1);
+});
