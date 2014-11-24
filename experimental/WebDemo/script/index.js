e6c8949f47cbf41623eb97541a4c3d01f9269159
Raffprta <raffprta@gmail.com>
2014-11-24 22:58:38 +0000

    Fix bad animation glitches on slow image rendering on JavaScript loaded images. Used quick JS caching solution

--- experimental/WebDemo/script/index.js
+++ experimental/WebDemo/script/index.js
@@ -1,11 +1,32 @@
 // Simple script for slider transitions.
 
-// The images directory.
-var directory = "images/";
-// Image array.
-var slider_images = ["slide_1.png", "slide_2.png", "slide_3.png", "slide_6.png", "slide_4.png", "slide_5.png", "slide_7.png"];
 // The position it is at currently in the slider.
 var transition_at = 0;
+
+/** Bug fix for slow internet connection or slow page serving:
+ * such as the Uni webspace - preload all of the images using Javascript
+ * and cache them to the browser.
+ *
+ * Images loaded from 1 -> n+1
+ *
+ * -- Apologies for quick ugly code, it was 11pm when I realised this --
+ * @param no_images The number of images to be loaded
+ * @param img_name the prefix name of the image such as slide_
+ * @param img_ext the file extension such as .png
+ * @param directory where the images are loaded from like images/
+ * @returns An Image array
+ */
+var preload = function(no_images, img_name, img_ext, directory){
+    var images = [];
+    for(var i = 0; i < no_images; i++) {
+        images[i] = new Image();
+        images[i].src = directory + img_name + (i+1) + img_ext;
+    }
+    return images;
+}
+
+// Image array.
+var slider_images = preload(7, "slide_", ".png", "images/");
 
 var slider_increment = function(offset){
     // Get the next slider image.
@@ -15,15 +36,15 @@ var slider_increment = function(offset){
         transition_at = slider_images.length - 1;
     }
     // JQuery animation!
-    $("#slider").slideDown("slow", function() {
-        $(this).fadeOut( "slow", function() {
-            $(this).attr("src", directory + slider_images[transition_at]);
-            $(this).fadeIn( "slow", function() {
-                // The slider image is faded out, and then faded in.
-            });
-        });
+    $("#slider").fadeOut("slow", function() {
 
+        $(this).attr("src", slider_images[transition_at].src);
+
+        $(this).fadeIn("slow", function() {
+
+        });
     });
+
 }
 
 // When the left button is clicked then go to the previous image.
