09e09c45328decc7ec1872cc393554a649b761e5
Raffprta <raffprta@gmail.com>
2015-02-20 20:04:35 +0000

    Added Ivy's Login and Security XML as an activity as well as fixed Intent structure

---
+++ androidapp/app/src/main/java/uk/ac/ncl/team19/lloydsapp/SecurityActivity.java
@@ -0,0 +1,33 @@
+package uk.ac.ncl.team19.lloydsapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * @author Raffaello Perrotta, XML creation by Ivy Tong
+ */
+public class SecurityActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.security_page);
+
+        // Set button listener.
+        findViewById(R.id.securityButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // UI effects.
+                v.getBackground().setColorFilter(0xe0f47521, PorterDuff.Mode.SRC_ATOP);
+                v.invalidate();
+                /// TODO Login auth. is done here.
+                Intent securityIntent = new Intent(SecurityActivity.this, MainMenuActivity.class);
+                startActivity(securityIntent);
+            }
+        });
+    }
+
+}
