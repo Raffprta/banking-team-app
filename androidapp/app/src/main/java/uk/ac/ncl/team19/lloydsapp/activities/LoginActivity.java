8aaab459eff8f689bb97eecd10d3e4273e51922e
Raffprta <raffprta@gmail.com>
2015-03-09 16:31:44 +0000

    Client side error detection added for login and security page

--- androidapp/app/src/main/java/uk/ac/ncl/team19/lloydsapp/activities/LoginActivity.java
+++ androidapp/app/src/main/java/uk/ac/ncl/team19/lloydsapp/activities/LoginActivity.java
@@ -1,17 +1,19 @@
 package uk.ac.ncl.team19.lloydsapp.activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.widget.EditText;
 
 import uk.ac.ncl.team19.lloydsapp.R;
+import uk.ac.ncl.team19.lloydsapp.dialogs.CustomDialog;
 import uk.ac.ncl.team19.lloydsapp.utils.general.GraphicsUtils;
 
 /**
  * @author Raffaello Perrota, XML by Ivy Tong
  */
-public class LoginActivity extends Activity{
+public class LoginActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +24,33 @@ public class LoginActivity extends Activity{
         findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // Check if the user entered anything.
+                String username = ((EditText)findViewById(R.id.userID)).getText().toString();
+                String password = ((EditText)findViewById(R.id.userPass)).getText().toString();
+                
+                // Display error if nothing was entered.
+                if(username == null || password == null || username.length() <= 0 || password.length() <= 0){
+                    Bundle b = new Bundle();
+                    b.putString(getString(R.string.custom_bundle), getString(R.string.err_empty_login));
+                    b.putString(getString(R.string.custom_type_bundle), getString(R.string.custom_colour_type_red));
+                    CustomDialog custom = new CustomDialog();
+                    custom.setArguments(b);
+                    custom.show(getSupportFragmentManager(), "Custom Dialog");
+                    GraphicsUtils.buttonClickEffectHide(v);
+                    return;
+                }
                 // UI effects.
                 GraphicsUtils.buttonClickEffectShow(v);
-                /// TODO Login auth. Login is done here.
+                /// Pass login credentials to the security intent for login.
+                Bundle b = new Bundle();
+                b.putString(getString(R.string.login_user_bundle), username);
+                b.putString(getString(R.string.login_pass_bundle), password);
+
                 Intent securityIntent = new Intent(LoginActivity.this, SecurityActivity.class);
+                // Pass the bundle to the Intent.
+                securityIntent.putExtras(b);
+                // Start the activity
                 startActivity(securityIntent);
             }
         });
--- androidapp/app/src/main/java/uk/ac/ncl/team19/lloydsapp/activities/SecurityActivity.java
+++ androidapp/app/src/main/java/uk/ac/ncl/team19/lloydsapp/activities/SecurityActivity.java
@@ -1,17 +1,19 @@
 package uk.ac.ncl.team19.lloydsapp.activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.widget.EditText;
 
 import uk.ac.ncl.team19.lloydsapp.R;
+import uk.ac.ncl.team19.lloydsapp.dialogs.CustomDialog;
 import uk.ac.ncl.team19.lloydsapp.utils.general.GraphicsUtils;
 
 /**
  * @author Raffaello Perrotta, XML creation by Ivy Tong
  */
-public class SecurityActivity extends Activity {
+public class SecurityActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,25 @@ public class SecurityActivity extends Activity {
             public void onClick(View v) {
                 // UI effects.
                 GraphicsUtils.buttonClickEffectShow(v);
+
+                String secondCharacter = ((EditText)findViewById(R.id.codeOne)).getText().toString();
+                String fourthCharacter = ((EditText)findViewById(R.id.codeTwo)).getText().toString();
+                String sixthCharacter = ((EditText)findViewById(R.id.codeThree)).getText().toString();
+
+                // Check if the user entered a character.
+
+                if(secondCharacter == null || fourthCharacter == null || sixthCharacter == null
+                   || secondCharacter.length() <= 0 || fourthCharacter.length() <= 0 || sixthCharacter.length() <= 0){
+                    Bundle b = new Bundle();
+                    b.putString(getString(R.string.custom_bundle), getString(R.string.err_empty_sec));
+                    b.putString(getString(R.string.custom_type_bundle), getString(R.string.custom_colour_type_red));
+                    CustomDialog custom = new CustomDialog();
+                    custom.setArguments(b);
+                    custom.show(getSupportFragmentManager(), "Custom Dialog");
+                    GraphicsUtils.buttonClickEffectHide(v);
+                    return;
+                }
+
                 /// TODO Login auth. is done here.
                 Intent securityIntent = new Intent(SecurityActivity.this, MainMenuActivity.class);
                 startActivity(securityIntent);
