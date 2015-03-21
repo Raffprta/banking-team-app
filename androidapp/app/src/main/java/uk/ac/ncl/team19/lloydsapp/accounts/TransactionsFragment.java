f2c56a65e31feb26f60e01d4f33cd267040a9b6a
Yessengerey Bolatov <yes.murz@gmail.com>
2015-03-21 13:54:43 +0000

    Progress in Transaction history page. Expandable list added with random values for now

--- androidapp/app/src/main/java/uk/ac/ncl/team19/lloydsapp/accounts/TransactionsFragment.java
+++ androidapp/app/src/main/java/uk/ac/ncl/team19/lloydsapp/accounts/TransactionsFragment.java
@@ -5,8 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.app.ExpandableListActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
 
 import uk.ac.ncl.team19.lloydsapp.R;
+
 
 /**
  * @author Yessengerey Bolatov (XML), Raffaello Perrotta
@@ -14,15 +24,86 @@ import uk.ac.ncl.team19.lloydsapp.R;
  */
 public class TransactionsFragment extends Fragment {
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         final View transactionsView = inflater.inflate(R.layout.transaction_history_page, container, false);
+        ExpandableListView elv = (ExpandableListView) transactionsView.findViewById(R.id.expandableListView);
+        elv.setAdapter(new ExpandableListAdapter());
 
         return transactionsView;
     }
+
+    public class ExpandableListAdapter extends BaseExpandableListAdapter {
+
+        private String[] groups = { "People Names", "Dog Names", "Cat Names", "Fish Names" };
+
+        private String[][] children = {
+                { "Arnold", "Barry", "Chuck", "David" },
+                { "Ace", "Bandit", "Cha-Cha", "Deuce" },
+                { "Fluffy", "Snuggles" },
+                { "Goldy", "Bubbles" }
+        };
+
+        @Override
+        public int getGroupCount() {
+            return groups.length;
+        }
+
+        @Override
+        public int getChildrenCount(int i) {
+            return children[i].length;
+        }
+
+        @Override
+        public Object getGroup(int i) {
+            return groups[i];
+        }
+
+        @Override
+        public Object getChild(int i, int i1) {
+            return children[i][i1];
+        }
+
+        @Override
+        public long getGroupId(int i) {
+            return i;
+        }
+
+        @Override
+        public long getChildId(int i, int i1) {
+            return i1;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+            TextView textView = new TextView(TransactionsFragment.this.getActivity());
+            textView.setText(getGroup(i).toString());
+            return textView;
+        }
+
+        @Override
+        public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+            TextView textView = new TextView(TransactionsFragment.this.getActivity());
+            textView.setText(getChild(i, i1).toString());
+            return textView;
+        }
+
+        @Override
+        public boolean isChildSelectable(int i, int i1) {
+            return true;
+        }
+
+    }
+
 
     @Override
     public String toString(){
