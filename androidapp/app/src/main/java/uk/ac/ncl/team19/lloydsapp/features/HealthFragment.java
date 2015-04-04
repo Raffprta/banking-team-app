22357c9e8298cfe66663662cb714d97f8d6e8445
Raffprta <raffprta@gmail.com>
2015-04-04 20:09:02 +0100

    initial commit. Added methods to retrieve list of accounts and transactions pertaining to them in health page

--- androidapp/app/src/main/java/uk/ac/ncl/team19/lloydsapp/features/HealthFragment.java
+++ androidapp/app/src/main/java/uk/ac/ncl/team19/lloydsapp/features/HealthFragment.java
@@ -8,16 +8,31 @@ import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 import uk.ac.ncl.team19.lloydsapp.R;
+import uk.ac.ncl.team19.lloydsapp.api.APIConnector;
+import uk.ac.ncl.team19.lloydsapp.api.datatypes.BankAccount;
+import uk.ac.ncl.team19.lloydsapp.api.response.APIResponse;
+import uk.ac.ncl.team19.lloydsapp.api.response.AccountDetailsResponse;
+import uk.ac.ncl.team19.lloydsapp.api.response.TransactionsResponse;
+import uk.ac.ncl.team19.lloydsapp.api.utility.ErrorHandler;
 import uk.ac.ncl.team19.lloydsapp.dialogs.CustomDialog;
 import uk.ac.ncl.team19.lloydsapp.utils.general.Constants;
 import uk.ac.ncl.team19.lloydsapp.utils.general.GraphicsUtils;
 
 /**
- * @author Raffaello Perrota, XML by Yessengerey Bolatov.
+ * @author Raffaello Perrotta, XML by Yessengerey Bolatov.
  * @author Dale Whinham - simplify Bundle key access
  *
  * A class which will hold all information pertaining to the account health status and allow the user
@@ -25,12 +40,80 @@ import uk.ac.ncl.team19.lloydsapp.utils.general.GraphicsUtils;
  */
 public class HealthFragment extends Fragment {
 
+    private List<BankAccount> accounts = null;
+    private ProgressBar progressBarAPI;
+    private GridLayout loadedView;
+    private TextView errorOnHealth;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         View healthView = inflater.inflate(R.layout.account_health_page, container, false);
+
+        final ProgressBar progressBar = (ProgressBar) healthView.findViewById(R.id.progressBar);
+        progressBarAPI = (ProgressBar) healthView.findViewById(R.id.progressBarAPI);
+        errorOnHealth = (TextView) healthView.findViewById(R.id.errorOnHealth);
+        loadedView = (GridLayout) healthView.findViewById(R.id.accountHealthGridLayout);
+        loadedView.setVisibility(View.INVISIBLE);
+
+
+        // Make shared preferences object
+        final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        // Make a call to the API.
+        final APIConnector ac = new APIConnector(getActivity());
+
+        // Get a list of all of the bank accounts belonging to the user
+        ac.getAccountDetails(new Callback<AccountDetailsResponse>() {
+
+            @Override
+            public void success(AccountDetailsResponse accountDetailsResponse, Response response) {
+
+                // Remove progress bar
+                progressBar.setVisibility(View.INVISIBLE);
+
+                if (accountDetailsResponse.getStatus() == APIResponse.Status.SUCCESS) {
+                    // Store accounts
+                    accounts = accountDetailsResponse.getAccounts();
+
+                    // Show message if no bank accounts returned
+                    if (accounts.size() == 0) {
+                        errorOnHealth.setText(R.string.account_info_no_accounts);
+                        return;
+                    }
+
+                    // Reinstate progress bar
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    // If there were bank accounts, now attempt to get transactions from them.
+                    String from = sp.getString(Constants.SP_GOALS_START, null);
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+                    Date fromDate = null;
+
+                    try {
+                        fromDate = sdf.parse(from);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+
+                    getTransactionsList(ac, accounts, fromDate, new Date());
+
+                }else{
+                    ErrorHandler.fail(getFragmentManager(), accountDetailsResponse.getErrorMessage());
+                    progressBarAPI.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void failure(RetrofitError retrofitError) {
+                // Handle error
+                ErrorHandler.fail(getActivity(), getFragmentManager(), retrofitError);
+                progressBarAPI.setVisibility(View.GONE);
+            }
+
+        });
 
         // Get the Health Bar.
         final ProgressBar hpBar = (ProgressBar) healthView.findViewById(R.id.progressBar);
@@ -58,8 +141,6 @@ public class HealthFragment extends Fragment {
         ProgressBar donateBar = (ProgressBar)healthView.findViewById(R.id.donateProgressBar);
 
 
-        // Make shared preferences object
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
         // If we are not donating, hide all views.
         if(!sp.getBoolean(Constants.SP_GOALS_DONATE, false)){
@@ -111,6 +192,43 @@ public class HealthFragment extends Fragment {
         });
 
         return healthView;
+
+    }
+
+    /**
+     * Helper method to get a list of transactions from each account.
+     * @param ac The APIConnector
+     * @param accounts The list of accounts to get the transactions from.
+     * @param from The date from
+     * @param to The date to - this will typically be "now".
+     */
+    private void getTransactionsList(APIConnector ac, List<BankAccount> accounts, Date from, Date to){
+
+        for(BankAccount account : accounts){
+            ac.getTransactions(account.getId(), from, to, new Callback<TransactionsResponse>() {
+
+                @Override
+                public void success(TransactionsResponse transactionsResponse, Response response) {
+                    if (transactionsResponse.getStatus() == APIResponse.Status.SUCCESS) {
+                        // On Success
+                        progressBarAPI.setVisibility(View.GONE);
+                        loadedView.setVisibility(View.VISIBLE);
+                    }else{
+                        progressBarAPI.setVisibility(View.GONE);
+                    }
+
+                }
+
+                @Override
+                public void failure(RetrofitError retrofitError) {
+                    // Handle error
+                    ErrorHandler.fail(getActivity(), getFragmentManager(), retrofitError);
+                    progressBarAPI.setVisibility(View.GONE);
+                }
+
+            });
+
+        }
 
     }
 
