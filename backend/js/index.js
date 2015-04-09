fe50a8a538cf0fd0054420f3cb7db5dbd16fe583
aziz-1 <i3z@live.ca>
2015-04-09 05:25:02 +0300

    about page creation

--- backend/js/index.js
+++ backend/js/index.js
@@ -26,7 +26,8 @@ var preload = function(no_images, img_name, img_ext, directory){
 }
 
 // Image array.
-var slider_images = preload(7, "slide_", ".png", "images/");
+var slider_images = preload(5, "slide_", ".png", "images/");
+
 
 var slider_increment = function(offset){
     // Get the next slider image.
@@ -56,3 +57,99 @@ $("#goLeft").click(function() {
 $("#goRight").click(function() {
     slider_increment(1);
 });
+
+
+slider_images_2 = preload(3, "sslide_", ".png", "images/");
+
+var slider_increment_2 = function(offset){
+    // Get the next slider image.
+    transition_at = (transition_at + offset) % slider_images_2.length;
+    // If the number falls below 0 (index out of bounds) set to the last array index.
+    if(transition_at < 0){
+        transition_at = slider_images_2.length - 1;
+    }
+    // JQuery animation!
+    $("#slider2").fadeOut("slow", function() {
+
+        $(this).attr("src", slider_images_2[transition_at].src);
+
+        $(this).fadeIn("slow", function() {
+
+        });
+    });
+
+}
+
+// When the left button is clicked then go to the previous image.
+$("#goLeft2").click(function() {
+    slider_increment_2(-1);
+});
+
+// When the right button is clicked then go to the next image.
+$("#goRight2").click(function() {
+    slider_increment_2(1);
+});
+
+slider_images_g = preload(4, "gslide_", ".png", "images/");
+
+var slider_increment_g = function(offset){
+    // Get the next slider image.
+    transition_at = (transition_at + offset) % slider_images_g.length;
+    // If the number falls below 0 (index out of bounds) set to the last array index.
+    if(transition_at < 0){
+        transition_at = slider_images_g.length - 1;
+    }
+    // JQuery animation!
+    $("#gslider").fadeOut("slow", function() {
+
+        $(this).attr("src", slider_images_g[transition_at].src);
+
+        $(this).fadeIn("slow", function() {
+
+        });
+    });
+
+}
+
+// When the left button is clicked then go to the previous image.
+$("#ggoLeft").click(function() {
+    slider_increment_g(-1);
+});
+
+// When the right button is clicked then go to the next image.
+$("#ggoRight").click(function() {
+    slider_increment_g(1);
+});
+
+slider_images_e = preload(4, "eslide_", ".png", "images/");
+
+var slider_increment_e = function(offset){
+    // Get the next slider image.
+    transition_at = (transition_at + offset) % slider_images_e.length;
+    // If the number falls below 0 (index out of bounds) set to the last array index.
+    if(transition_at < 0){
+        transition_at = slider_images_e.length - 1;
+    }
+    // JQuery animation!
+    $("#eslider").fadeOut("slow", function() {
+
+        $(this).attr("src", slider_images_e[transition_at].src);
+
+        $(this).fadeIn("slow", function() {
+
+        });
+    });
+
+}
+
+// When the left button is clicked then go to the previous image.
+$("#egoLeft").click(function() {
+    slider_increment_e(-1);
+});
+
+// When the right button is clicked then go to the next image.
+$("#egoRight").click(function() {
+    slider_increment_e(1);
+});
+
+
